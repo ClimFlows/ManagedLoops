cc39ff99cb8143f4bf55c772df1ee63a01cb15d4
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-02 20:12:01 +0100

    Increase coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,18 @@
-using ManagedLoops: @loops
+using ManagedLoops: @loops, @vec, @unroll
+using Test
 
-@loops function test!(_, a, b)
-    let irange = eachindex(a,b)
-        for i in irange
-            a[i] = exp(b[i])
+@loops function test!(_, a, b, c)
+    let irange = eachindex(a, b, c)
+        @vec for i in irange
+            a[i] = @unroll sum( c[i]^n for n in 1:4)
+            b[i] = c[i] + c[i]^2 + c[i]^3 + c[i]^4
         end
     end
 end
 
-a = randn(100)
-b = randn(100)
-
-test!(nothing, a, b)
+@testset "Macros" begin
+    c = randn(100)
+    a, b = similar(c), similar(c)
+    test!(nothing, a, b, c)
+    @test a ≈ b
+end
