30d7baa7118ee5c25599a93d3909ee77b59a3069
Thomas Dubos <dubos@llmd.ipsl.fr>
2024-09-27 18:41:33 +0200

    Final fix

--- ext/ManagedLoopsAdaptExt.jl
+++ ext/ManagedLoopsAdaptExt.jl
@@ -8,12 +8,13 @@ using Adapt: Adapt, adapt
 # when adapting a manager to a target manager, the target manager replaces the original manager
 # otherwise we put 'missing'
 Adapt.adapt_structure(to, mgr::LoopManager) = adapt_structure_mgr(to, mgr)
-adapt_structure_mgr(to, mgr) = Adapt.adapt_storage(to, mgr) # not sure this is the right thing to do
+# Fallback implementation of `Adapt.adapt_storage(to, x)` is `Adapt.adapt_structure(to, x)`
+adapt_structure_mgr(to, mgr) = Adapt.adapt_storage(to, mgr)
 adapt_structure_mgr(to::LoopManager, mgr) = to
 
 # adapting an AbstractArray to the CPU returns an array
 Adapt.adapt_storage(::HostManager, x) = adapt_to_host(x)
-#adapt_to_host(x) = x
+adapt_to_host(x) = x
 adapt_to_host(x::Array) = x
 adapt_to_host(x::AbstractArray) = Array(x)
 
