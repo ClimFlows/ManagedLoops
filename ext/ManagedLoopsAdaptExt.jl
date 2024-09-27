f4ddd36ce80871c2b32f8c750ab96768f21d3c1f
Thomas Dubos <dubos@llmd.ipsl.fr>
2024-09-27 13:06:53 +0200

    Do not return `missing`

--- ext/ManagedLoopsAdaptExt.jl
+++ ext/ManagedLoopsAdaptExt.jl
@@ -7,8 +7,8 @@ using Adapt: Adapt, adapt
 
 # when adapting a manager to a target manager, the target manager replaces the original manager
 # otherwise we put 'missing'
-Adapt.adapt_structure(to, mgr::LoopManager) = adapt_structure_mgr(to)
-adapt_structure_mgr(_) = missing
-adapt_structure_mgr(to::LoopManager) = to
+Adapt.adapt_structure(to, mgr::LoopManager) = adapt_structure_mgr(to, mgr)
+adapt_structure_mgr(to, mgr) = Adapt.adapt_storage(to, mgr) # not sure this is the right thing to do
+adapt_structure_mgr(to::LoopManager, mgr) = to
 
 end
