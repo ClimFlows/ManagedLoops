2d34eda55a75ac074d644d11f1d7d7aba7a74019
Thomas Dubos <dubos@llmd.ipsl.fr>
2024-09-27 10:38:28 +0200

    `adapt(to, mgr)`

--- ext/ManagedLoopsAdaptExt.jl
+++ ext/ManagedLoopsAdaptExt.jl
@@ -5,4 +5,10 @@ using Adapt: adapt
 
 (mgr::LoopManager)(x) = adapt(mgr, x)
 
+# when adapting a manager to a target manager, the target manager replaces the original manager
+# otherwise we put 'missing'
+Adapt.adapt_structure(to, mgr::LoopManager) = adapt_structure_mgr(to)
+adapt_structure_mgr(_) = missing
+adapt_structure_mgr(to::LoopManager) = to
+
 end
